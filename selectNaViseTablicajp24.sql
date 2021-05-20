8aa649d12a469ca9cf61ee4591c29e0030b99f79
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-05-20 15:15:06 +0100

    insertServiser

--- selectNaViseTablicajp24.sql
+++ selectNaViseTablicajp24.sql
@@ -6,3 +6,12 @@ insert into smjer(naziv,trajanje)
 values ('Serviser',100);
 
 select * from smjer;
+
+# smjeru naziva Serviser postavite cijenu 2500 kn
+
+update smjer set cijena=2500 where sifra=3;
+
+
+select b.naziv as smjer, a.naziv as grupa
+from grupa a inner join smjer b
+on a.smjer=b.sifra;
